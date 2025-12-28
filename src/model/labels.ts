dd5054b2767e870e47cfe36b9023988a54e99dc3
Patricio Lopez Valiente <patriciolopezvaliente@gmail.com>
2025-12-28 18:09:44 -0300

    fix: favicon and messages

---
+++ src/model/labels.ts
@@ -0,0 +1,18 @@
+export const siteLabels = {
+  instagram: 'https://www.instagram.com/argot.colectivo/',
+  banner: '/// Buenos Aires, Argentina /// info@argot.com.ar ' + 
+    '/// Buenos Aires, Argentina /// info@argot.com.ar ' + 
+    '/// Buenos Aires, Argentina /// info@argot.com.ar ' + 
+    '/// Buenos Aires, Argentina /// info@argot.com.ar ',
+  description: {
+    intro: [
+      `ARGOT es un grupo de investigación interdisciplinar que explora la intersección entre arte, arquitectura y ciencia. ` + 
+      `Desde Argentina hacia el mundo, el colectivo inició su investigación en 2019 con el desarrollo de objetos a pequeña escala ` + 
+      `fabricados con materiales reciclados y pequeños ventiladores.`,
+      `Con el tiempo, estas indagaciones evolucionaron hacia instalaciones de escala en el paisaje urbano, expandiendo el trabajo ` + 
+      `de ARGOT hacia obras de expresión escenográfica en espacios públicos y proyectos colaborativos con actores de diferentes disciplinas.`,
+      `La expresión de las obras son el resultado parcial de un proceso de experimentación sobre intervenciones situadas a partir ` + 
+      `de la multiplicidad y economía de recursos tecnológicos y creativos.`
+    ],
+  }
+}
